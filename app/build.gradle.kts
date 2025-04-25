53d9330b5c2a2eb7747a12f3f93d2b9d17eb9d6a
harisov <harisov.mobile@gmail.com>
2025-04-25 10:18:35 +0300

    Up versionCode

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
     applicationId = "ru.internetcloud.whereami"
     minSdk = 25
     targetSdk = 35
-    versionCode = 1
-    versionName = "1.0"
+    versionCode = 3
+    versionName = "3.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
